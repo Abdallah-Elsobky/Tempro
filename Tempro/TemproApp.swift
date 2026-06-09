9f5ae03e2d7903a0c1b86993973679df48938638
Abdallah-Elsobky <abdullah442286@fsc.bu.edu.eg>
2026-06-09 16:50:36 -0700

    here we go again

---
+++ Tempro/TemproApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemproApp.swift
+//  Tempro
+//
+//  Created by Elsobky on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TemproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
